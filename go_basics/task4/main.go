f2daaf979c984aa6524584fe2e0a8ff1e6dfc83f
wzhen <wangzhen@17zhiliao.com>
2025-08-27 13:36:00 +0800

    go mod

--- go_basics/task4/main.go
+++ go_basics/task4/main.go
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package main
 
 import (
@@ -14,20 +13,3 @@ func main() {
 	routers.PostRouterInit(r)
 	r.Run()
 }
-=======
-package main
-
-import (
-	"task4/routers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-	routers.UserRouterInit(r)
-	routers.CommentTouterInit(r)
-	routers.PostRouterInit(r)
-	r.Run()
-}
->>>>>>> 0a25e3ccee0f1071177d78e3d62c3db86e3c70b4
