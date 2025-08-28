dd2e9bcd7fafb5265f57ffb07a56dfd61be45791
wzhen1020 <97860987+wzhen1020@users.noreply.github.com>
2025-08-29 03:23:37 +0800

    Update Binary.sol

--- solidity_basics/task1/Binary.sol
+++ solidity_basics/task1/Binary.sol
@@ -1,1 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract Binary {
+
+
+
+    function  search (int[] memory nums, int target) public pure returns (int num) {
+        uint len = nums.length;
+        if(len == 0){
+            return -1;
+        }
+        uint right = len -1;
+        uint left =  0;
+        int value = 0;
+        uint index = 0;
+        while (left <= right) 
+        {
+            index = (right - left)/2 + left;
+           value = nums[index];
+
+            if(right == 0){
+                break;
+            }
+            if(value > target){
+                right = index -1;
+                continue;
+            }
+
+            if(value < target){
+                left = index +1;
+                continue;
+            }
+
+            if(value == target){
+                return int(index);
+            }
+        }
+        return -1;
+          
+    }
+}
