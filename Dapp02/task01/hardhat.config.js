20268750ca9be4ccd2f90893de6ef1fed33ea3fb
wzhen <wangzhen@17zhiliao.com>
2025-09-24 18:22:51 +0800

    任务二

--- Dapp02/task01/hardhat.config.js
+++ Dapp02/task01/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      // Infura URL sepolia 获取地址：https://developer.metamask.io/key/active-endpoints
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      // 钱包KEY
+      accounts: [process.env.WALLET_PRIVATE_KEY]
+    }
+  }
 };
